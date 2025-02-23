a7c58a9d97415d4fbf7ce4cbfc9d57a44d7265fa
Atzel <axel.netrof@uanl.edu.mx>
2025-02-23 04:14:29 +0000

    calificaciones terminado

--- fase 1/calificaciones.py
+++ fase 1/calificaciones.py
@@ -1,1 +1,24 @@
-print("Hola mundo")
+calificaciones = 0
+contador = 0
+
+print('A continuación ingrese calificaciones para obtener un promedio, y escriba letras para finalizar')
+
+while True:
+    calificacion = (input('Calificación: '))
+    try:
+        calificacion = float(calificacion)
+    except ValueError:
+        break
+
+    if 0 <= calificacion <= 100:
+        calificaciones += calificacion
+        contador += 1
+        continue
+    else:
+        print('Los números válidos son del 0 al 100')
+
+if contador > 0:
+    promedio = calificaciones / contador
+    print(f'El promedio es: {promedio}')
+else:
+    print('No ingresó calificaciones')
