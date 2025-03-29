c38cd446399a8f86dc26e734953d4ab3f1506984
Atzel <axel.netrof@uanl.edu.mx>
2025-03-29 00:41:27 +0000

    dic de prod

---
+++ fase 2/dic.precios.py
@@ -0,0 +1,11 @@
+productos = {'leche': 23, 'aceite': 40, 'arroz': 28, 'huevo': 54 }
+
+prod = input('Producto: ')
+cant = int(input('Cantidad de unidades: '))
+
+precio = productos[prod] * cant
+
+try:    
+    print(f'Precio total: ${precio}')
+except ValueError:
+    print('Ingresó un producto no existente')
