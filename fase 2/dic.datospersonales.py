010a8acc7f7b125ebd3d19184b09e107c7ef5845
Atzel <axel.netrof@uanl.edu.mx>
2025-05-04 00:17:17 +0000

    3r problema

--- fase 2/dic.datospersonales.py
+++ fase 2/dic.datospersonales.py
@@ -1,10 +1,17 @@
+def get_int(s):
+    while True:
+        try:
+            return int(input(s))
+        except ValueError:
+            print('Ingrese un número entero')
+
 print('Ingrese los siguientes datos')
 
 nombre = input('Nombre: ')
-edad = int(input('Edad: '))
+edad = get_int('Edad: ')
 direccion = input('Dirección: ')
-telefono = int(input('Teléfono: '))
+telefono = get_int('Teléfono: ')
 
 persona = {'nombre': nombre, 'edad': edad, 'direccion': direccion, 'telefono': telefono}
 
-print(f'{nombre} tiene {edad} años, con domicilio en {direccion} y su número telefónico es {telefono}.')
+print(f'{persona["nombre"]} tiene {persona["edad"]} años, con domicilio en {persona["direccion"]} y su número telefónico es {persona["telefono"]}.')
